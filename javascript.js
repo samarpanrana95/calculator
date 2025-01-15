d9990b69e59fcddc9cfef287eb1f7cf6774543c1
Samarpan <samarpanrana95@gmail.com>
2025-01-15 20:50:02 +0545

    Add calculator-ui in html with basic css styling
    Added calculator-ui content in index.html with dummy content
    to test styles

--- javascript.js
+++ javascript.js
@@ -1,1 +1,37 @@
-alert("works");
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function diff(num1, num2) {
+  return num1 - num2;
+}
+
+function mul(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return ((num1 * 10) / (num2 * 10)).toFixed(2);
+}
+
+function operate(num1, num2, operator) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = diff(num1, num2);
+      break;
+    case "*":
+      result = mul(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+    default:
+      result = "ERROR";
+      break;
+  }
+  return result;
+}
