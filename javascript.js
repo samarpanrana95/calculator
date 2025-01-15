cbb83dbd1a366f383497b957d7ecff16d6a90d46
Samarpan <samarpanrana95@gmail.com>
2025-01-15 21:47:24 +0545

    Add basic get initalNumber input and get operation input features

--- javascript.js
+++ javascript.js
@@ -35,3 +35,73 @@ function operate(num1, num2, operator) {
   }
   return result;
 }
+
+let initialNumber, finalNumber, currentOperation;
+
+let buttons = document.querySelectorAll(".button");
+let buttonsContainer = document.querySelector(".buttons-container");
+buttonsContainer.addEventListener("click", (e) => {
+  let target = e.target;
+  let targetClassList = target.classList;
+  if (targetClassList.contains("clear-button")) {
+    console.log("cleared");
+  } else if (targetClassList.contains("delete-button")) {
+    console.log("deleted");
+  } else if (targetClassList.contains("equals-button")) {
+    console.log("equals");
+  } else if (targetClassList.contains("operation-button")) {
+    let pressedOperation = getOperation(target);
+    updatePressedOperation(pressedOperation, target);
+  } else if (targetClassList.contains("number-button")) {
+    let pressedNumber = getNumber(target);
+    updateInitialNumber(pressedNumber);
+  }
+});
+
+function getOperation(target) {
+  let targetText = target.textContent;
+  switch (targetText) {
+    case "÷":
+      return "divide";
+    case "×":
+      return "mul";
+    case "+":
+      return "add";
+    case "-":
+      return "diff";
+  }
+}
+
+function updatePressedOperation(pressedOperation, target) {
+  currentOperation = pressedOperation;
+  updatePressedOperationDom(currentOperation, target);
+}
+
+function updatePressedOperationDom(currentOperation, target) {
+  let displayPrevText = document.querySelector(".display-prev-text");
+  displayPrevText.textContent = initialNumber + target.textContent;
+  displayPrevText.style.visibility = "visible";
+  let displayMainText = document.querySelector(".display-main-text");
+  displayMainText.textContent = 0;
+  displayMainText.style.visibility = "hidden";
+}
+
+function getNumber(target) {
+  return target.textContent;
+}
+
+function updateInitialNumber(pressedNumber) {
+  if (typeof initialNumber == "string") {
+    initialNumber += pressedNumber;
+  } else {
+    initialNumber = pressedNumber;
+  }
+
+  updateInitialNumberDom();
+}
+
+function updateInitialNumberDom() {
+  let displayMainText = document.querySelector(".display-main-text");
+  displayMainText.style.visibility = "visible";
+  displayMainText.textContent = initialNumber;
+}
